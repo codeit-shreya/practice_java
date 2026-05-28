9acb9ff848d52b11e0ec904517ec5384a022eafc
codeit-shreya <shreyasingh22may@gmail.com>
2026-05-28 20:41:26 +0530

    basic/learn.java and names.txt added

---
+++ basics/Learn.java
@@ -0,0 +1,8 @@
+public class Learn {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("How are you doing");
+        System.out.println("Welcome");
+        System.out.println("Yo");
+    }
+}
